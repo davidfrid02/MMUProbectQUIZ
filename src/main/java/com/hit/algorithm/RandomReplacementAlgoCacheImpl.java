60e19b67bf5a141779a8a04c04e6944afc92a392
Tomer <tomeraa@gmail.com>
2016-07-30 21:29:23 +0300

    uploaded project

---
+++ src/main/java/com/hit/algorithm/RandomReplacementAlgoCacheImpl.java
@@ -0,0 +1,28 @@
+package com.hit.algorithm;
+
+public class RandomReplacementAlgoCacheImpl<K,V> implements IAlgoCache<K,V> {
+	
+	RandomReplacementAlgoCacheImpl(int capacity)
+	{
+		
+	}
+	
+	@Override
+	public V getElement(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V putElement(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeElement(K key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/test/com/hit/algorithm/IAlgoCacheTest.java
@@ -0,0 +1,10 @@
+package com.hit.algorithm;
+
+public class IAlgoCacheTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
